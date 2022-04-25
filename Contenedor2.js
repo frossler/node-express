98c3c379cd1919bc86752f3ea700c36866408154
Fernando Rossler <fernando_rossler@FYBSISTEMAS.local>
2022-04-25 10:13:26 -0300

    changes

--- Contenedor2.js
+++ Contenedor2.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 
+
+
+
 class Contenedor {
     constructor() {
         this.id = undefined,
@@ -8,19 +11,17 @@ class Contenedor {
             this.thumbnail = undefined
     }
 
-
-
     save = (title, price, thumbnail, obj) => {
         try {
-            if (fs.existsSync('./products.txt')) {
-                const data = JSON.parse(fs.readFileSync('./products.txt', 'utf-8'))
+            if (fs.existsSync('./desafio.txt')) {
+                const data = JSON.parse(fs.readFileSync('./desafio.txt', 'utf-8'))
                 const lastProd = data[data.length - 1].id
                 this.title = title
                 this.price = price
                 this.thumbnail = thumbnail
                 this.id = lastProd + 1
                 data.push(obj)
-                fs.writeFileSync('./products.txt', `${JSON.stringify(data)}`)
+                fs.writeFileSync('./desafio.txt', `${JSON.stringify(data)}`)
             } else {
                 const array = []
                 this.title = title
@@ -28,7 +29,7 @@ class Contenedor {
                 this.thumbnail = thumbnail
                 this.id = 1
                 array.push(obj)
-                fs.writeFileSync('./products.txt', `${JSON.stringify(array)}`)
+                fs.writeFileSync('./desafio.txt', `${JSON.stringify(array)}`)
             }
 
         } catch (error) {
@@ -38,26 +39,24 @@ class Contenedor {
         console.log('Archivo guardado!')
     }
 
+
     getByID = (id) => {
-        const data = fs.readFileSync('./products.txt', 'utf-8')
-        const match = JSON.parse(data).find(x => x.id === id)
-
-			return console.log('getByID', match)
-	}
-
+        const data = fs.readFileSync('./desafio.txt', 'utf-8')
+        console.log('getById', JSON.parse(data).find(x => x.id === id))
+    }
 
     getAll = () => {
-        const data = fs.readFileSync('./products.txt', 'utf-8')
+        const data = fs.readFileSync('./desafio.txt', 'utf-8')
         console.log(data)
     }
 
     deleteById =  (id) => {
         try {
-            const data = fs.readFileSync('./products.txt', 'utf-8')
+            const data = fs.readFileSync('./desafio.txt', 'utf-8')
             const newArray = JSON.parse(data)
              const filteredArray = newArray.filter(x => x.id !== id)
             
-            fs.writeFileSync('./products.txt', `${JSON.stringify(filteredArray)}`)
+            fs.writeFileSync('./desafio.txt', `${JSON.stringify(filteredArray)}`)
             console.log('Producto eliminado', filteredArray)
         } catch (error) {
             console.log(error)
@@ -65,22 +64,21 @@ class Contenedor {
     }
 
     deleteAll = async () => {
-        await fs.promises.writeFile('./products.txt', ``)
+        await fs.promises.writeFile('./desafio.txt', ``)
         console.log('Productos eliminados')
     }
 }
 
-
 // Instancio y guardo los productos nuevos
 const contenedor = new Contenedor()
-// contenedor.save('cartuchera', 100, 'www.libreria.com', contenedor)
-// contenedor.save('lapiz', 20, 'www.libreria.com', contenedor)
-// contenedor.save('hoja', 5, 'www.libreria.com',contenedor)
+contenedor.save('cartuchera', 100, 'www.dsfsd.com', contenedor)
+contenedor.save('lapiz', 20, 'www.dsfsdsss.com', contenedor)
+contenedor.save('hoja', 5, 'www.dsfsd111.com',contenedor)
 //Obtengo todos los productos
 // contenedor.getAll()
 //Obtengo el producto con ID 2
-contenedor.getByID(2)
+// contenedor.getByID(2)
 //Elimino el producto con ID 2
-// contenedor.deleteById(2)
+contenedor.deleteById(2)
 //Metodo para borrar todo
 // contenedor.deleteAll()
