9cae2ed5f4265da8389c74f816ffa6e8e445735f
Fernando Rossler <fernando_rossler@FYBSISTEMAS.local>
2022-04-28 17:30:30 -0300

    Changes

--- Contenedor.js
+++ Contenedor.js
@@ -1,86 +1,113 @@
-const fs = require('fs')
+const fs = require('fs');
 
 class Contenedor {
-    constructor() {
-        this.id = undefined,
-            this.title = undefined,
-            this.price = undefined,
-            this.thumbnail = undefined
+
+    constructor(nombreArchivo) {
+
+        this.fileName = nombreArchivo;
+        this.contenido = [];
+
+    //  obtengo el contenido del archivo al instanciar la clase
+        this.leerArchivo()
+
     }
 
+    leerArchivo() {
 
-
-    save = (title, price, thumbnail, obj) => {
         try {
-            if (fs.existsSync('./products.txt')) {
-                const data = JSON.parse(fs.readFileSync('./products.txt', 'utf-8'))
-                const lastProd = data[data.length - 1].id
-                this.title = title
-                this.price = price
-                this.thumbnail = thumbnail
-                this.id = lastProd + 1
-                data.push(obj)
-                fs.writeFileSync('./products.txt', `${JSON.stringify(data)}`)
-            } else {
-                const array = []
-                this.title = title
-                this.price = price
-                this.thumbnail = thumbnail
-                this.id = 1
-                array.push(obj)
-                fs.writeFileSync('./products.txt', `${JSON.stringify(array)}`)
+
+            if (fs.existsSync(this.fileName)) {
+                const data = fs.readFileSync(this.fileName, 'utf-8');
+                this.contenido = JSON.parse(data);
             }
 
         } catch (error) {
-            console.log(error)
+            console.log('Error al leer el archivo', error);
         }
-
-        console.log('Archivo guardado!')
     }
 
-    getByID = (id) => {
-        const data = fs.readFileSync('./products.txt', 'utf-8')
-        const match = JSON.parse(data).find(x => x.id === id)
+    escribirArchivo(contenido) {
 
-			return console.log('getByID', match)
-	}
-
-
-    getAll = () => {
-        const data = fs.readFileSync('./products.txt', 'utf-8')
-        console.log(data)
-    }
-
-    deleteById =  (id) => {
         try {
-            const data = fs.readFileSync('./products.txt', 'utf-8')
-            const newArray = JSON.parse(data)
-             const filteredArray = newArray.filter(x => x.id !== id)
-            
-            fs.writeFileSync('./products.txt', `${JSON.stringify(filteredArray)}`)
-            console.log('Producto eliminado', filteredArray)
+            fs.writeFileSync(this.fileName, JSON.stringify(contenido))
         } catch (error) {
-            console.log(error)
+            console.log('Error al escribir el archivo', error);
         }
     }
 
-    deleteAll = async () => {
-        await fs.promises.writeFile('./products.txt', ``)
-        console.log('Productos eliminados')
+    getAll() {
+        return this.contenido;
     }
+
+    getById(id) {
+
+        let producto = {}
+        producto = this.contenido.find(element => element.id == id);
+        if (!producto) {
+            producto = null
+        }
+        return producto
+    }
+
+    save(producto) {
+
+        //  obtengo ultimo id + 1
+        const id = this.contenido.length + 1;
+        producto["id"] = id;
+        //  actualizo el contenido con el nuevo producto
+        this.contenido.push(producto);
+        //  Grabo el archivo nuevamente
+        this.escribirArchivo(this.contenido);
+
+        return `El id del objeto añadido es ${id}`
+    }
+
+    deleteById(id) {
+
+        const contenidoNuevo = this.contenido.filter(element => element.id !== id)
+        this.escribirArchivo(contenidoNuevo);
+
+    }
+
+    deleteAll() {
+        const contentido = [];
+        this.contenido = contentido
+        this.escribirArchivo(this.contenido);
+    }
+
 }
 
 
-// Instancio y guardo los productos nuevos
-const contenedor = new Contenedor()
-contenedor.save('cartuchera', 100, 'www.libreria.com', contenedor)
-contenedor.save('lapiz', 20, 'www.libreria.com', contenedor)
-contenedor.save('hoja', 5, 'www.libreria.com',contenedor)
-//Obtengo todos los productos
-// contenedor.getAll()
-//Obtengo el producto con ID 2
-// contenedor.getByID(2)
-//Elimino el producto con ID 2
-// contenedor.deleteById(2)
-//Metodo para borrar todo
-// contenedor.deleteAll()
+const contenedor = new Contenedor('./products.txt');
+
+// contenedor.save({
+//     title: "escuadra",
+//     price: 80.20,
+//     thumbnail: "escuadra.jpg"
+// });
+
+// contenedor.save({
+//     title: "regla",
+//     price: 141.23,
+//     thumbnail: "regla.jpg"
+// });
+
+// contenedor.save({
+//     title: "compas",
+//     price: 11.23,
+//     thumbnail: "compas.jpg"
+// });
+
+// contenedor.save({
+//     title: "transportador",
+//     price: 16.23,
+//     thumbnail: "transportador.jpg"
+// });
+
+// contenedor.save({
+//     title: "lapicera",
+//     price: 1.23,
+//     thumbnail: "lapicera.jpg"
+// });
+
+console.log('Todos los productos',contenedor.getAll());
