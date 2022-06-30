bd0adcc7287be4ea6cef29a438592462d3f8aa9d
Fernando Rossler <fernando_rossler@FYBSISTEMAS.local>
2022-06-30 17:02:44 -0300

    Desafio 9

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,54 @@
 const socket = io();
 
+const { schema, denormalize } = normalizr;
+
+const chat = document.querySelector('#chat');
+const compressionDiv = document.querySelector('#compression');
+
+const author = new schema.Entity('authors');
+
+const message = new schema.Entity('messages', {
+  author: author,
+});
+
+const messagesSchema = new schema.Array(message);
+
+async function fetchMessagesAndUpdateUI() {
+  const normalizedMsg = await (await fetch('/api/messages')).json();
+
+  const messages = denormalize(normalizedMsg.result, messagesSchema, normalizedMsg.entities);
+
+  const template = Handlebars.compile(
+    '<span style="color: blue; font-weight: 600;">{{this.author.id}}: </span><span style="color: brown;">[{{this.date}}] </span><span style="color: green; font-style: italic;">{{this.text}}</span>'
+  );
+
+  const normalizedLength = JSON.stringify(normalizedMsg).length;
+  const messagesLength = JSON.stringify(messages).length;
+
+  console.log(normalizedLength, messagesLength);
+
+  const compression = Math.round(((messagesLength - normalizedLength) / messagesLength) * 100);
+
+  compressionDiv.textContent = `Compression: %${compression}`;
+
+  const elements = messages.map((message) => {
+    const li = document.createElement('li');
+    li.innerHTML = template(message);
+
+    return li;
+  });
+
+  chat.innerHTML = '';
+
+  elements.forEach((element) => {
+    chat.appendChild(element);
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetchMessagesAndUpdateUI();
+});
+
 const form = document.querySelector('#form');
 const titleInput = document.querySelector('#title');
 const priceInput = document.querySelector('#price');
@@ -7,80 +56,87 @@ const thumbnailInput = document.querySelector('#thumbnail');
 
 const table = document.querySelector('#table');
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-	if (titleInput.value && priceInput.value && thumbnailInput.value) {
-		const product = {
-			title: titleInput.value,
-			price: priceInput.value,
-			thumbnail: thumbnailInput.value,
-		};
+  if (titleInput.value && priceInput.value && thumbnailInput.value) {
+    const product = {
+      title: titleInput.value,
+      price: priceInput.value,
+      thumbnail: thumbnailInput.value,
+    };
 
-		socket.emit('add-product', product);
-		titleInput.value = '';
-		priceInput.value = '';
-		thumbnailInput.value = '';
-	}
+    socket.emit('add-product', product);
+    titleInput.value = '';
+    priceInput.value = '';
+    thumbnailInput.value = '';
+  }
 });
 
-socket.on('update-products', product => {
-	const template = Handlebars.compile(
-		"<td>{{title}}</td><td>{{price}}</td><td><img src={{thumbnail}} style='width:50px;'></img></td>"
-	);
+socket.on('update-products', (product) => {
+  const template = Handlebars.compile(
+    "<td>{{title}}</td><td>{{price}}</td><td><img src={{thumbnail}} style='width:50px;'></img></td>"
+  );
 
-	const tr = document.createElement('tr');
+  const tr = document.createElement('tr');
 
-	tr.innerHTML = template(product);
+  tr.innerHTML = template(product);
 
-	table.appendChild(tr);
+  table.appendChild(tr);
 });
 
 const messageForm = document.querySelector('#messages');
 const emailInput = document.querySelector('#email');
 const messageInput = document.querySelector('#message');
-
-const chat = document.querySelector('#chat');
+const nameInput = document.querySelector('#name');
+const lastNameInput = document.querySelector('#lastname');
+const aliasInput = document.querySelector('#alias');
+const ageInput = document.querySelector('#age');
+const avatarInput = document.querySelector('#avatar');
 
 const errors = document.querySelector('#errors');
 
-messageForm.addEventListener('submit', e => {
-	e.preventDefault();
-	errors.innerHTML = '';
+const inputs = [
+  emailInput,
+  messageInput,
+  nameInput,
+  lastNameInput,
+  aliasInput,
+  ageInput,
+  avatarInput,
+];
 
-	if (!emailInput.value) {
-		const error = document.createElement('p');
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  errors.innerHTML = '';
 
-		error.innerText = 'Please enter an email';
-		errors.appendChild(error);
-	}
+  inputs.forEach((input) => {
+    if (!input.value) {
+      const error = document.createElement('p');
+      error.innerHTML = `${input.name} es requerido`;
 
-	if (!messageInput.value) {
-		const error = document.createElement('p');
+      errors.append(error);
+    }
+  });
 
-		error.innerText = 'Please enter a message';
-		errors.appendChild(error);
-	}
+  if (messageInput.value && emailInput.value) {
+    const message = {
+      author: {
+        id: emailInput.value,
+        nombre: nameInput.value,
+        apellido: lastNameInput.value,
+        alias: aliasInput.value,
+        edad: ageInput.value,
+        avatar: avatarInput.value,
+      },
+      text: messageInput.value,
+    };
+    socket.emit('message', message);
 
-	if (messageInput.value && emailInput.value) {
-		const message = {
-			email: emailInput.value,
-			message: messageInput.value,
-		};
-		socket.emit('message', message);
-		emailInput.value = '';
-		messageInput.value = '';
-	}
+    inputs.forEach((input) => (input.value = ''));
+  }
 });
 
-socket.on('message', message => {
-	const template = Handlebars.compile(
-		'<span style="color: blue; font-weight: 600;">{{this.email}}: </span><span style="color: brown;">[{{this.date}}] </span><span style="color: green; font-style: italic;">{{this.message}}</span>'
-	);
-
-	const li = document.createElement('li');
-
-	li.innerHTML = template(message);
-
-	chat.appendChild(li);
+socket.on('message', (message) => {
+  fetchMessagesAndUpdateUI();
 });
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const http = require('http');
 const { Server } = require('socket.io');
 const Productos = require('./Productos');
 const Mensajes = require('./Mensajes');
+const { default: faker } = require('@faker-js/faker');
+const { normalize, schema } = require('normalizr');
+const util = require('util');
 
 const app = express();
 const server = http.createServer(app);
@@ -27,16 +30,50 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const products = new Productos();
-
 const mensajes = new Mensajes();
+
+// Normalizr
+
+const author = new schema.Entity('authors');
+
+const message = new schema.Entity('messages', {
+  author: author,
+});
+
+const messagesSchema = new schema.Array(message);
+
+function print(data) {
+  console.log(util.inspect(data, false, 12, true));
+}
+
+app.get('/api/messages', async (req, res) => {
+  const messages = await mensajes.getAll();
+
+  res.json(normalize(messages, messagesSchema))
+})
 
 app.get('/', async (req, res) => {
   const productos = await products.getAll();
 
-
   let messages = await mensajes.getAll();
 
+  // console.log(messages);
+  // print(normalize(messages, messagesSchema));
+
   res.render('main', { title: 'Productos', productos, messages });
+});
+
+app.get('/api/productos-test', async (req, res) => {
+  const productos = [1, 2, 3, 4, 5].map((id) => {
+    return {
+      id,
+      title: faker.commerce.product(),
+      price: faker.commerce.price(),
+      thumbnail: faker.image.image(),
+    };
+  });
+
+  res.render('random', { title: 'Productos Random', productos });
 });
 
 io.on('connection', (socket) => {
@@ -56,10 +93,19 @@ io.on('connection', (socket) => {
 
   socket.on('message', async (message) => {
     const data = {
-      email: message.email,
-      message: message.message,
-      date: new Date().toLocaleString(),
+      author: {
+        id: message.author.id,
+        nombre: message.author.nombre,
+        apellido: message.author.apellido,
+        alias: message.author.alias,
+        edad: message.author.edad,
+        avatar: message.author.avatar,
+      },
+      text: message.text,
+      date: new Date()
     };
+
+    // console.log(message);
 
     await mensajes.save(data);
 
@@ -72,7 +118,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ err, message: 'Something went wrong, sorry' });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3005;
 
 server.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto http://localhost:${PORT}`);
